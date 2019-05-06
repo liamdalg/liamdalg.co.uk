c6b4ddccac896c7d2262b85bbd57c8f1dd02d4c6
liamdalg <liamdalg99@gmail.com>
2019-05-06 23:21:12 +0100

    initial commit:
    + SASS
    + Typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Liam Dalgarno`,
+    description: `Personal blog built with Gatsby.`,
+    author: `@liamdalg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -31,4 +33,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import * as styles from '../styles/index.module.scss';
+
+/**
+  const IndexPage = () => (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1 className={styles.title}>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+**/
+
+interface IndexProps {}
+
+class IndexPage extends React.Component<IndexProps, {}> {
+  public render() {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hello World!</h1>
+        <p>Development in progress with Gatsby + SASS + TypeScript.</p>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
