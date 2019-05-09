bb84a1969d4c93ade44c06e93466cc40c53f32c7
liamdalg <liamdalg99@gmail.com>
2019-05-09 04:42:12 +0100

    remove old plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -59,14 +58,6 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /images/,
-        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
