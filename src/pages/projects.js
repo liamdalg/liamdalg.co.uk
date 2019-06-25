7c017700dc31b1a97d20ce63e96954508845c546
liamdalg <liamdalg99@gmail.com>
2019-06-25 13:48:28 +0100

    add project page and fix blog layout

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,12 +6,12 @@ import SEO from '../components/seo';
 const Blog = () => {
   const banner = (
     <div className="text-centered absolute-centered main-link">
-      <h1>Blog</h1>
+      <h1>Projects</h1>
     </div>
   );
   return (
     <Layout banner={banner} bannerHeight="300px">
-      <SEO title="Blog" />
+      <SEO title="Projects" />
     </Layout>
   );
 };
