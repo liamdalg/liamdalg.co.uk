740597197df7c27adb96b1ad925883eb2b5a12aa
liamdalg <liamdalg99@gmail.com>
2019-09-16 00:19:31 +0100

    barebones markdown page enumeration

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Blog = () => {
+const Blog = ({ data }) => {
+  const edges = data.allMarkdownRemark.edges;
+  console.log(edges);
   const banner = (
     <div className="text-centered absolute-centered main-link">
       <h1>Blog</h1>
@@ -11,8 +14,33 @@ const Blog = () => {
   return (
     <Layout banner={banner} bannerHeight="400px">
       <SEO title="Blog" />
+      {edges.map(edge => (
+        <div>
+          <Link to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+          <p dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
+        </div>
+      ))}
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(format: HTML, pruneLength: 200)
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
