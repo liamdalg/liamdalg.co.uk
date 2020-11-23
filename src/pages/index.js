17352704076dd5d75a0eb2b6dd4487d4419aac3a
Liam Dalgarno <liamdalg99@gmail.com>
2020-11-23 05:47:25 +0000

    badly display posts on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,51 +7,49 @@ import '../styles/index.scss';
 import SEO from '../components/seo';
 
 export const query = graphql`
-  query IndexImageQuery {
-    fileName: file(relativePath: { eq: "me.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 400, maxHeight: 400) {
-          ...GatsbyImageSharpFluid
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          date
+          description
+          title
         }
       }
     }
   }
 `;
 
-class IndexPage extends React.Component {
-  render() {
-    const splash = (
-      <div className="text-centered">
-        <div className="absolute-centered splash-info">
-          <h1 className="splash-title">
-            <span className="accent-text">l</span>iam
-            <span className="accent-text">d</span>alg
-          </h1>
-          <h4 className="splash-subtitle">Under construction!</h4>
-        </div>
-        <div
-          onClick={() =>
-            document
-              .querySelector('#firstContainer')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-          className="arrow-down absolute-centeredX clickable"
-        />
+const IndexPage = ({ data }) => {
+  const splash = (
+    <div className="text-centered">
+      <div className="absolute-centered splash-info">
+        <h1 className="splash-title">
+          <span className="accent-text">l</span>iam
+          <span className="accent-text">d</span>alg
+        </h1>
+        <h4 className="splash-subtitle">Under construction!</h4>
       </div>
-    );
-    return (
-      <Layout bannerHeight="100vh" noSpacer banner={splash}>
-        <SEO title="Home" />
-        <div id="firstContainer" className="container home-about">
-          <h1 className="container-title">About Me</h1>
-          <div className="home-about-left">
-            <Img
-              className="rounded-image about-image"
-              fluid={this.props.data.fileName.childImageSharp.fluid}
-              alt="A picture of me!"
-            />
-          </div>
-          <div className="home-about-right justified-text spaced-text">
+      <div
+        onClick={() =>
+          document
+            .querySelector('#firstContainer')
+            .scrollIntoView({ behavior: 'smooth' })
+        }
+        className="arrow-down absolute-centeredX clickable"
+      />
+    </div>
+  );
+  return (
+    <Layout bannerHeight="100vh" noSpacer banner={splash}>
+      <SEO title="Home" />
+      <div id="firstContainer" className="container home-about">
+        <h1 className="container-title">About Me</h1>
+        <div className="span-container justified-text spaced-text">
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a
             ipsum est. Vivamus ullamcorper sem erat, eu sodales metus finibus
             eu. Aliquam erat nunc, sagittis nec faucibus non, pulvinar a lectus.
@@ -62,17 +60,31 @@ class IndexPage extends React.Component {
             congue sapien. Sed nec neque est. In consequat arcu sit amet feugiat
             laoreet. Phasellus molestie, dolor non semper mattis, orci eros
             semper felis, vitae feugiat dui arcu ut leo.
-          </div>
+          </p>
+          <p>
+            Fusce sagittis nulla sit amet posuere dapibus. Praesent facilisis
+            elementum orci ac sollicitudin. Quisque quis posuere mauris. Donec
+            tristique justo id tellus convallis auctor. Vivamus a consequat
+            risus. Sed ipsum nunc, hendrerit ac suscipit in, iaculis sit amet
+            purus. Nullam nec nisi ac diam consectetur molestie a ac sem.
+          </p>
         </div>
-        <div className="container home-projects">
-          <h1 className="container-title">Projects</h1>
-        </div>
-        <div className="container home-projects">
-          <h1 className="container-title">Recent Blog Posts</h1>
-        </div>
-      </Layout>
-    );
-  }
-}
+      </div>
+      <div className="container home-projects">
+        <h1 className="container-title">Recent Blog Posts</h1>
+        <ul class="span-container">
+          {data.allMarkdownRemark.nodes.map(node => (
+            <li>
+              <a href={node.fields.slug}>{node.frontmatter.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="container home-projects">
+        <h1 className="container-title">Projects</h1>
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
